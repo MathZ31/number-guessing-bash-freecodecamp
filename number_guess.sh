11e64d2aa19fcc59b36dd57ded7effc94e0f28ec
MathZ31 <114731612+MathZ31@users.noreply.github.com>
2025-03-16 19:54:06 +0100

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -1,70 +1,63 @@
-#!/bin/bash
+#! /bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
-
-# Create database and table if they don't exist
-psql --username=freecodecamp --dbname=postgres <<EOSQL
-CREATE DATABASE number_guess;
-EOSQL
-
-psql --username=freecodecamp --dbname=number_guess <<EOSQL
-CREATE TABLE IF NOT EXISTS users (
-  user_id SERIAL PRIMARY KEY,
-  username VARCHAR(22) UNIQUE NOT NULL,
-  games_played INT DEFAULT 0,
-  best_game INT DEFAULT NULL
-);
-EOSQL
-
-# Ask for username
+#Number Guessing Game
+echo -e "\n\n~~Welcome to Number Guessing Game~~\n"
+#query database
+PSQL="psql --username=freecodecamp --dbname=number_guessing_game_db -Atc"
+#read username
 echo "Enter your username:"
 read USERNAME
-
-# Check if user exists
-USER_DATA=$($PSQL "SELECT user_id, games_played, best_game FROM users WHERE username='$USERNAME'")
-
-if [[ -z "$USER_DATA" ]]; then
-  echo "Welcome, $USERNAME! It looks like this is your first time here."
-  $PSQL "INSERT INTO users(username) VALUES('$USERNAME')"
+#Allow usernames only between 3 and 22 characters.(Uppercase or lowercase letters with digits and the symbols: - and _ are allowed as well)
+while ! [[ "$USERNAME" =~ ^[[:alpha:][:digit:]_-]{3,22}$ ]]
+do
+  echo "Sorry, '$USERNAME' name is not allowed, try please another one:"
+  read USERNAME
+done
+#Search current user in database
+USER_ID="$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME';")"
+if [[ -z $USER_ID ]]
+then
+  #insert new user
+  INSERT_NEW_USER="$($PSQL "INSERT INTO users(username) VALUES('$USERNAME');")"
+  #get new user id
   USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
-  GAMES_PLAYED=0
-  BEST_GAME=NULL
+  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
 else
-  IFS='|' read USER_ID GAMES_PLAYED BEST_GAME <<< "$USER_DATA"
-  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  #get data from existing user
+  GET_USER_GAMES_DATA="$($PSQL "SELECT COUNT(*), MIN(result) FROM games WHERE user_id = $USER_ID;")"
+  echo $GET_USER_GAMES_DATA | while IFS="|" read -a Read
+  do
+    echo -e "\nWelcome back, $USERNAME! You have played ${Read[0]} games, and your best game took ${Read[1]} guesses.\n"
+  done
 fi
-
-# Generate random number
-SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
-TRIES=0
-
-# Start game
-echo "Guess the secret number between 1 and 1000:"
-while true; do
-  read GUESS
-  ((TRIES++))
-
-  # Validate input
-  if ! [[ "$GUESS" =~ ^[0-9]+$ ]]; then
+#start game
+# generate a random number between 1 and 1000
+SECRET_NUMBER=$(( $RANDOM % 1001 ))
+#get user's number
+echo -e "\nGuess the secret number between 1 and 1000:"
+read USER_GUESS
+#initiate number of guesses
+NUMBER_OF_GUESSES=1
+while [[ $USER_GUESS != $SECRET_NUMBER ]]
+do
+  if [[ ! $USER_GUESS =~ ^[0-9]+$ ]]
+  then
     echo "That is not an integer, guess again:"
-    continue
-  fi
-
-  # Compare guess with secret number
-  if (( GUESS < SECRET_NUMBER )); then
-    echo "It's higher than that, guess again:"
-  elif (( GUESS > SECRET_NUMBER )); then
-    echo "It's lower than that, guess again:"
+    read USER_GUESS
   else
-    echo "You guessed it in $TRIES tries. The secret number was $SECRET_NUMBER. Nice job!"
-    break
+    if [[ $USER_GUESS > $SECRET_NUMBER ]]
+    then
+      NUMBER_OF_GUESSES=$(($NUMBER_OF_GUESSES + 1))
+      echo "It's lower than that, guess again:"
+      read USER_GUESS
+    else
+      NUMBER_OF_GUESSES=$(($NUMBER_OF_GUESSES + 1))
+      echo "It's higher than that, guess again:"
+      read USER_GUESS
+    fi
   fi
 done
-
-# Update database
-NEW_GAMES_PLAYED=$((GAMES_PLAYED + 1))
-$PSQL "UPDATE users SET games_played=$NEW_GAMES_PLAYED WHERE user_id=$USER_ID"
-
-if [[ -z "$BEST_GAME" || TRIES -lt BEST_GAME ]]; then
-  $PSQL "UPDATE users SET best_game=$TRIES WHERE user_id=$USER_ID"
-fi
+#insert new game
+INSERT_NEW_GAME="$($PSQL "INSERT INTO games(user_id, result) VALUES($USER_ID, '$NUMBER_OF_GUESSES');")"
+#congratulation message
+echo -e "\nYou guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!\n"
